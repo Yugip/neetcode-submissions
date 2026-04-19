cbdaafe93efa61cdb3a091bba53f24cf5f9eed02
Ethan Fields <86757117+Yugip@users.noreply.github.com>
2026-04-19 19:59:49 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        numSet = set(nums);
+
+        if len(numSet) == len(nums):
+            return False;
+        else:
+            return True;
+
+        
